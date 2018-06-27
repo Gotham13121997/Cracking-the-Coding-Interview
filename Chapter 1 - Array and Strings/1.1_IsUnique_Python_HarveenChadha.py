fd52c22a15e0cdebaf7b2def4c6b438483d62d9c
harveenchadha <harveensinghchadha@infosys.com>
2018-06-27 23:36:09 +0530

    Add: 1.1_IsUnique

---
+++ Chapter 1 - Array and Strings/1.1_IsUnique_Python_HarveenChadha.py
@@ -0,0 +1,33 @@
+
+
+# Problem 1.1 IsUnique
+
+
+# When You can use another datastrucure
+
+a = input("Please enter your input:\n") 
+dict = []
+flag =0
+for i in range(len(a)):
+	if a[i] in dict:
+		flag=1
+		break
+	else:
+		dict.append(a[i])
+if flag==1:
+	print("Not unique")
+else:
+	print("Unique")
+	
+# Part 2: When you cannot use any other datastructure
+
+for i in range(len(a)):
+	for j in range(i+1,len(a)):
+		if(a[j] == a[i]):
+			flag=1
+			break
+if flag==1:
+	print("Not unique")
+else:
+	print("Unique")		
+	
